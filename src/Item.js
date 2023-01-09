93f9398f10a283ba966d9b26bf56b068a0fbb877
EricLam404 <lameric9888@gmail.com>
2023-01-08 22:49:45 -0500

    Added img and cost

--- src/Item.js
+++ src/Item.js
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './App.css';
 
-function Item() {
+function Item(props) {
     useEffect(() => {
         fetchItems();
     }, []);
 
     const { id } = useParams();
-    const [item, setItem] = useState({data: {item: ''}});
+    const [cost, setCost] = useState(0);
+    const [item, setItem] = useState({
+        data: {
+            item: '',
+            images: {icon: ""}
+        }
+    });
     const fetchItems = async () => {
         const data = await fetch(`https://fortnite-api.com/v2/cosmetics/br/${id}`);
         const item = await data.json();
-        console.log(item.data.name)
+        console.log(item)
         setItem(item);
     }
 
     return (
       <div className="item">
           <h1>{item.data.name}</h1>
+          <img src={`${item.data.images.icon}`}/>
+          <div>{cost}</div>
       </div>
     );
   }
