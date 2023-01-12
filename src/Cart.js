9ece7738964b8a55c678077ef4a988e5127a7b67
EricLam404 <lameric9888@gmail.com>
2023-01-11 20:46:01 -0500

    Added shopping cart

---
+++ src/Cart.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+function Item(props) {
+    const [cart, setCart] = useState([]);
+    return (
+      <div className="cart">
+          <h1>Shopping Cart</h1>
+          {cart.map(item => {
+                return(
+                <h1>{item.name}</h1>
+                )
+            })}
+      </div>
+    );
+  }
+
+export default Item;
