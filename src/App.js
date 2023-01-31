fde1e3c5b63379c2eccbf987d3fc9c548e3ead48
EricLam404 <lameric9888@gmail.com>
2023-01-30 21:22:31 -0500

    Added description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './css/App.css';
 function App() {
   return (
     <div className="App">
-        <div>Fortnite Item Shop</div>
+        <div className='title'>Fortnite Item Shop</div>
+        <div>This is the daily item shop for Fortnite. You can add as many items as you want. Click the cart to show the total</div>
     </div>
   );
 }
